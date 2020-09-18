0accfef1e7d0c5d2c748fc109d38cfc7d601e0ba
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2020-09-18 12:19:38 +0300

    default tranformer, xcode 12

---
+++ DatabaseKit/DefaultTransformer.swift
@@ -0,0 +1,34 @@
+//
+//  DefaultTransformer.swift
+//  DatabaseKit
+//
+//  Created by Ilya Kuznetsov on 9/18/20.
+//  Copyright © 2020 Ilya Kuznetsov. All rights reserved.
+//
+
+import Foundation
+
+@objc(DefaultTransformer)
+class DefaultTransformer: ValueTransformer {
+    override class func transformedValueClass() -> AnyClass {
+        return NSData.self
+    }
+
+    override open func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let value = value as? Data else {
+            return nil
+        }
+        return NSKeyedUnarchiver.unarchiveObject(with: value)
+    }
+
+    override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let value = value else {
+            return nil
+        }
+        return NSKeyedArchiver.archivedData(withRootObject: value)
+    }
+}
