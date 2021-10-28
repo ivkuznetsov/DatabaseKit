f4cbbea261a72950dee6fa40fa02a722b7d83d9b
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2021-10-28 10:29:33 +0300

    removed UIKit reference

--- DatabaseKit/DatabaseKit.h
+++ DatabaseKit/DatabaseKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Ilya Kuznetsov. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for DatabaseKit.
 FOUNDATION_EXPORT double DatabaseKitVersionNumber;
